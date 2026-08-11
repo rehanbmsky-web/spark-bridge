d452a887c8c255011885f6a51d70ae900a8190ab
rehanbmsky-web <rehanbmsky@gmail.com>
2026-08-11 17:13:01 +0600

    Add files via upload

---
+++ api/webhook.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode === 'subscribe' && token === 'MY_SECRET_TOKEN') {
+      res.setHeader('Content-Type', 'text/plain');
+      return res.status(200).send(challenge);
+    } else {
+      return res.status(403).send('Verification failed');
+    }
+  }
+
+  if (req.method === 'POST') {
+    return res.status(200).send('EVENT_RECEIVED');
+  }
+
+  return res.status(405).send('Method Not Allowed');
+}
